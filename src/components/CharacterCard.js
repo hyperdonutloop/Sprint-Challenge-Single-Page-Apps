95b9de13777a97854187f432e197ad6a77f600fc
donutwizard666 <thebeardedwarrior@icloud.com>
2019-10-18 08:26:00 -0700

    fetched list of characters and successfully rendered them to the screen

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+
+
+  return (
+    <div className="character-card">
+      <div className="character-list">
+        <h2>Name: {props.name}</h2>
+        <p>Status: {props.status}</p>
+        <p>Species: {props.species}</p>
+        <p>Gender: {props.gender}</p>
+      </div>
+
+    </div>
+
+  )
 }
